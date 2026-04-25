de87a4c8c44fc2b2b84c4d63b61ce00d492f059a
Dania Khan <142868029+daniakhan123@users.noreply.github.com>
2026-04-25 15:42:25 +0500

    Create data.py

---
+++ data.py
@@ -0,0 +1,8 @@
+def get_next_stop():
+    return "Your next stop is Hyderabad in 2 hours."
+
+def get_traffic():
+    return "Traffic is clear ahead."
+
+def send_message():
+    return "Message sent to dispatcher: I will be delayed."
