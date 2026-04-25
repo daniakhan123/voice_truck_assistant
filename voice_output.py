9f5ff551b30318614798cd567cc79fbe0bb4b81e
Dania Khan <142868029+daniakhan123@users.noreply.github.com>
2026-04-25 16:03:40 +0500

    Update voice_output.py

--- voice_output.py
+++ voice_output.py
@@ -1,6 +1,6 @@
 from elevenlabs import generate, play, set_api_key
 
-set_api_key("YOUR_API_KEY")
+set_api_key("sk_2cf01b1942751f34ee1bf0c1ff16c8a1f50dcbbf31ad53db")
 
 def speak(text):
     audio = generate(
